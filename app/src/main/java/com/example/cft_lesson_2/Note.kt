4fda26d3d24b098a63fd328506a5bf09ef707c59
Zabolotnikoff <zaband@list.ru>
2020-12-22 00:16:11 +0700

    Serializable + Lambda

--- app/src/main/java/com/example/cft_lesson_2/Note.kt
+++
@@ -1,3 +0,0 @@
-package com.example.cft_lesson_2
-
-class Note (val title: String, val description: String = "На момент публикации информация отсутствует")
--- app/src/main/java/com/example/cft_lesson_2/NoteListActivity.kt
+++ app/src/main/java/com/example/cft_lesson_2/NoteListActivity.kt
@@ -6,39 +6,42 @@ import android.os.Bundle
 //import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cft_lesson_2.model.entity.Note
 //import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.note_list_activity.*
 
-class NoteListActivity: AppCompatActivity() {
+class NoteListActivity : AppCompatActivity() {
 
-   private val noteList = listOf(
-       Note("Научиться создавать приложения под Android", "Ох уж эта наука..."),
-       Note("Сходить в магазин", "Шесть пива, две рыбки. ...Хотя, пива всё таки восемь."),
-       Note("Ответить на вопросы из чатика", "Ну..., мне надо подумать"),
-       Note("Открыть Америку", "Снаряжаем экспедицию Христофора Колумба"),
-       Note("Закрыть Америку", "Проверить координаты... Первая, третья, восьмая, четырнадцатая - пуск! "),
-       Note("Сдать сессию"),
-       Note("Накормить хомяка зёрнами", "Ням-ням... Хрум-хрум..."),
-       Note("Накормить кота хомяком", "Мяу... Дайте лучше рыбки")
-   )
+    private val noteList = listOf(
+        Note("Научиться создавать приложения под Android", "Ох уж эта наука..."),
+        Note("Сходить в магазин", "Шесть пива, две рыбки. ...Хотя, пива всё таки восемь."),
+        Note("Ответить на вопросы из чатика", "Ну..., мне надо подумать"),
+        Note("Открыть Америку", "Снаряжаем экспедицию Христофора Колумба"),
+        Note(
+            "Закрыть Америку",
+            "Проверить координаты... Первая, третья, восьмая, четырнадцатая - пуск! "
+        ),
+        Note("Сдать сессию"),
+        Note("Накормить хомяка зёрнами", "Ням-ням... Хрум-хрум..."),
+        Note("Накормить кота хомяком", "Мяу... Дайте лучше рыбки")
+    )
+
+    val adapter = NoteListAdapter{note ->
+        val intent = Intent(this@NoteListActivity, DetailNoteActivity::class.java)
+        intent.putExtra("Note", note)
+        startActivity(intent)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.note_list_activity)
 
-        val adapter = NoteListAdapter()
+        val layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
 
         reciclerView.adapter = adapter
-        reciclerView.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        reciclerView.layoutManager = layoutManager
 
         adapter.setNoteList(noteList)
-        adapter.setNoteListener(object : NoteListAdapter.NoteListener {
-            override fun onNoteClick(note: Note) {
-                val intent = Intent(this@NoteListActivity, DetailNoteActivity::class.java)
-                intent.putStringArrayListExtra("Note", arrayListOf(note.title, note.description))
-                startActivity(intent)
-             }
-        })
-
     }
 }
